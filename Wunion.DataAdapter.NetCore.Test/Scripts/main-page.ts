d7e49dfe40a71da0096dc0b6e6c7ad7399eaa41a
巽翎君 <cnzhnet@hotmail.com>
2020-05-06 14:29:04 +0800

    node_modules

---
+++ Wunion.DataAdapter.NetCore.Test/Scripts/main-page.ts
@@ -0,0 +1,3 @@
+﻿class MainPage {
+    constructor() { }
+}
---
+++ Wunion.DataAdapter.NetCore.Test/gulpfile.js
@@ -0,0 +1,31 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+
+const gulp = require('gulp');
+const del = require('del');
+
+var paths = {
+    scripts: [
+        'Scripts/**/*.js',
+        'Scripts/**/*.ts',
+        'Scripts/**/*.map'
+        //'!Scripts/**/*.d.ts'
+    ],
+    lib: []
+};
+
+// 执行清理任务.
+gulp.task('clean', function () {
+    return del(['wwwroot/js/**/*', 'Scripts/**/*.js', 'Scripts/**/*.map']);
+});
+
+//gulp.task('lib', function (done) {
+//    gulp.src(paths.lib).pipe(gulp.dest("wwwroot/lib/crypto-js"));
+//    done(); 
+//});
+
+gulp.task('default', function (done) {
+    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/js'));
+    //del("Scripts/**/*.js");
+    //del("Scripts/**/*.map");
+    done();
+});
